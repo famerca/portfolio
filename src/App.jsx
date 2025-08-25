8b66169bed342131c333a06f2e1681615da36841
Fabian Flores <famercado24@gmail.com>
2025-08-25 18:33:49 -0400

    timeline experience

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./in.jsx"
 
 function App() {
   const [load, upadateLoad] = useState(true);
--- src/components/Home/Timeline.jsx
+++ src/components/Home/Timeline.jsx
@@ -1,50 +1,35 @@
+import { useTranslation } from 'react-i18next';
 
 const Timeline = () => {
-  return (
+
+    const { t } = useTranslation();
+
+    return (
         <ul class="timeline">
             <li class="timeline-item">
-                <h5 class="fw-bold">Our company starts its operations</h5>
-                <p class="text-muted mb-2">11 March 2020</p>
-                <p class="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                    necessitatibus adipisci, ad alias, voluptate pariatur officia
-                    repellendus repellat inventore fugit perferendis totam dolor
-                    voluptas et corrupti distinctio maxime corporis optio?
-                </p>
+                <h5 class="fw-bold">{t("works.landing.title")}</h5>
+                <p class="text-muted mb-2">{t("works.landing.start")} - {t("works.landing.end")}</p>
+                <p class="">{t("works.landing.description")}</p>
             </li>
 
             <li class="timeline-item">
-            <h5 class="fw-bold">First customer</h5>
-            <p class="text-muted mb-2 ">19 March 2020</p>
-            <p class="">
-                Quisque ornare dui nibh, sagittis egestas nisi luctus nec. Sed
-                aliquet laoreet sapien, eget pulvinar lectus maximus vel.
-                Phasellus suscipit porta mattis.
-            </p>
+                <h5 class="fw-bold">{t("works.xtetic.title")}</h5>
+                <p class="text-muted mb-2">{t("works.xtetic.start")} - {t("works.xtetic.end")}</p>
+                <p class="">{t("works.xtetic.description")}</p> 
             </li>
 
             <li class="timeline-item">
-            <h5 class="fw-bold">Our team exceeds 10 people</h5>
-            <p class="text-muted mb-2">24 June 2020</p>
-            <p>
-                Orci varius natoque penatibus et magnis dis parturient montes,
-                nascetur ridiculus mus. Nulla ullamcorper arcu lacus, maximus
-                facilisis erat pellentesque nec. Duis et dui maximus dui aliquam
-                convallis. Quisque consectetur purus erat, et ullamcorper sapien
-                tincidunt vitae.
-            </p>
+                <h5 class="fw-bold">{t("works.inglesenminutos.title")}</h5>
+                <p class="text-muted mb-2">{t("works.inglesenminutos.start")} - {t("works.inglesenminutos.end")}</p>
+                <p class="">{t("works.inglesenminutos.description")}</p>
             </li>
 
             <li class="timeline-item">
-            <h5 class="fw-bold">Earned the first million $!</h5>
-            <p class="text-muted mb-2">15 October 2020</p>
-            <p>
-                Nulla ac tellus convallis, pulvinar nulla ac, fermentum diam. Sed
-                et urna sit amet massa dapibus tristique non finibus ligula. Nam
-                pharetra libero nibh, id feugiat tortor rhoncus vitae. Ut suscipit
-                vulputate mattis.
-            </p>
+                <h5 class="fw-bold">{t("works.integragym.title")}</h5>
+                <p class="text-muted mb-2">{t("works.integragym.start")} - {t("works.integragym.end")}</p>
+                <p class="">{t("works.integragym.description")}</p>
             </li>
+
         </ul>
        
 );
