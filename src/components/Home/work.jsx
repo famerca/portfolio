ce2ec9cec2c1e6b33cfdc67bd721eaef31995a7c
Fabian Flores <famercado24@gmail.com>
2025-08-27 12:55:16 -0400

    timeline tooltip

--- src/components/Home/work.jsx
+++ src/components/Home/work.jsx
@@ -1,9 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import Image from "react-bootstrap/Image";
 import img from "../../Assets/home-main.svg";
-import { React, Nextjs } from "../CustomIcons";
+import { React, Nextjs, Sanity, Php, Js, Tailwind, Aws, Mysql, Nodejs, Express, Playwright, Assembly } from "../CustomIcons";
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
+
 
 const Work = ({ active }) => {
 
@@ -21,6 +22,134 @@ const Work = ({ active }) => {
         </Tooltip>
     );
 
+    const reactElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('React')}
+                    >
+                        <span>
+                            <React h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    const nextjsElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('Next.js')}
+                    >
+                        <span>
+                            <Nextjs h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+    const sanityElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('Sanity')}
+                    >
+                        <span>
+                            <Sanity h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    const jsElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('JavaScript')}
+                    >
+                        <span>
+                            <Js h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+    
+    const playwrightElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('Playwright')}
+                    >
+                        <span>
+                            <Playwright h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    const nodejsElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('Node.js')}
+                    >
+                        <span>
+                            <Nodejs h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    const expressElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('Express.js')}
+                    >
+                        <span>
+                            <Express h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+    
+    const awsElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('AWS')}
+                    >
+                        <span>
+                            <Aws h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    const assemblyElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('AssemblyAI')}
+                    >
+                        <span>
+                            <Assembly h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    const mysqlElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('MySQL')}
+                    >
+                        <span>
+                            <Mysql h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    const phpElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('PHP')}
+                    >
+                        <span>
+                            <Php h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+    
+    const TailwindElement = <OverlayTrigger
+                        placement="top"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip('Tailwind')}
+                    >
+                        <span>
+                            <Tailwind h={24} w={24} />
+                        </span>
+                    </OverlayTrigger>
+
+    
+    const skills = {
+        "landing" : [reactElement, nextjsElement, sanityElement, TailwindElement],
+        "xtetic" : [nodejsElement, expressElement, playwrightElement, awsElement],
+        "inglesenminutos" : [jsElement, phpElement, mysqlElement, assemblyElement],
+        "integragym" : [jsElement, phpElement, mysqlElement],
+        "artofgalaxy" : [reactElement, TailwindElement]
+    }
+
     if (active === '') {
         content = <>
             <h2>
@@ -28,24 +157,10 @@ const Work = ({ active }) => {
             </h2>
             <ul className="work-skills">
                 <li>
-                    {/* 2. Llama a renderTooltip con el texto deseado */}
-                    <OverlayTrigger
-                        placement="top"
-                        delay={{ show: 250, hide: 400 }}
-                        overlay={renderTooltip('React')}
-                    >
-                        <React h={24} w={24} />
-                    </OverlayTrigger>
+                    {reactElement}
                 </li>
                 <li>
-                    {/* 3. Haz lo mismo para Next.js */}
-                    <OverlayTrigger
-                        placement="top"
-                        delay={{ show: 250, hide: 400 }}
-                        overlay={renderTooltip('Next.js')}
-                    >
-                        <Nextjs h={24} w={24} />
-                    </OverlayTrigger>
+                    {nextjsElement}
                 </li>
             </ul>
             <ul className="work-duties">
@@ -57,29 +172,7 @@ const Work = ({ active }) => {
                 {t("works.duties")}
             </h2>
             <ul className="work-skills">
-                <li>
-                    <OverlayTrigger
-                        placement="top"
-                        delay={{ show: 250, hide: 400 }}
-                        overlay={renderTooltip('React')}
-                    >
-                        <span>
-                            <React h={24} w={24} />
-                        </span>
-                    </OverlayTrigger>
-                </li>
-                <li>
-                    <OverlayTrigger
-                        placement="top"
-                        delay={{ show: 250, hide: 400 }}
-                        overlay={renderTooltip('Next.js')}
-                    >
-                        <span>
-                            <Nextjs h={24} w={24} />
-                        </span>
-                    </OverlayTrigger>
-                </li>
-             
+                {skills[active].map((item, index) => <li key={index}>{item}</li>)}
             </ul>
             <ul className="work-duties">
                 {t(key, { returnObjects: true }).map((item, index) => <li key={index}>{item}</li>)}
